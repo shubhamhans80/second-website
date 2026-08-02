51e03a4c772eb187a5b41a7f9e97a62f3dcc68ea
shubhamhans148 <ganeshhans934@gmail.com>
2026-08-02 18:25:31 +0530

    Project res

--- app.py
+++ app.py
@@ -8,8 +8,12 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/pchapter2')
+def chapter2():
+    return render_template('index2.html')
+
 # 2. अबाउट पेज रूट (About Page Route)
-@app.route('/about')
+@app.route('/pabout')
 def about():
     return render_template('about.html')
 
@@ -17,6 +21,24 @@ def about():
 @app.route('/robots.txt')
 def robots_txt():
     return send_from_directory(os.path.join(app.root_path, 'static'), 'robots.txt')
+
+
+
+# git status
+# git add . / git add e:\main\seekhocoding  
+# git commit -m "Project updated with new features"
+# git push origin main
+ 
+
+# fresh upload starting se 
+
+# Remove-Item -Recurse -Force .git
+# git init
+# git add -A
+# git commit -m "Fresh initial commit"
+# git branch -M main
+# git remote add origin https://github.com/shubhamhans80/mysite.git
+# git push -u origin main --force
 
 if __name__ == '__main__':
     # debug=True रखने से कोड बदलते ही सर्वर अपने आप रीस्टार्ट हो जाता है
